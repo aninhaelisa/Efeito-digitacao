5c98402e0267fc5216acaea928b5ba6583919926
Aninha <103537945+aninhaelisa@users.noreply.github.com>
2023-05-14 09:44:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var string = "Feliz dias das Mães";
+var array = string.split("");
+var timer;
+function frameLooper () {
+  if (array.length > 0) {
+    document.getElementById("text").innerHTML += array.shift();
+  } else {
+    clearTimeout(timer);
+      }
+  loopTimer = setTimeout('frameLooper()',70);
+}
+frameLooper();
